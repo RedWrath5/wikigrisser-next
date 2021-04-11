fe655ec44ee3766f2415dc3516b2363c43076d6a
RedWrath5 <ezchess9@gmail.com>
2021-04-11 13:54:21 -0500

    Add marging to skill tops

--- components/heroes/SkillSection.tsx
+++ components/heroes/SkillSection.tsx
@@ -10,7 +10,7 @@ export function SkillSection({
 }) {
   return (
     <>
-      <div className="grid grid-cols-12 items-center mb-2">
+      <div className="grid grid-cols-12 items-center mt-2 mb-2">
         <div className="col-span-12 text-center sm:col-span-1">
           <div className="mb-3">
             <Image
