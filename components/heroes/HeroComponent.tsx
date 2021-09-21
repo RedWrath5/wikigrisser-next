d047c846c8225c882d47fad5d3124e390711b602
Самойленко Валерий Сергеевич - СРБ <samoylenko-vs@sigma.sbrf.ru>
2021-09-21 13:31:01 +0300

    added tabs and material section into hero page

--- components/heroes/HeroComponent.tsx
+++ components/heroes/HeroComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
 import { Hero } from "../../types/hero";
 import { BoundedColumn } from "../layout/BoundedColumn";
 import { ClassSection } from "./ClassSection";
@@ -7,8 +7,18 @@ import { HeroSkinCarousel } from "./HeroSkinCarousel";
 import { SkillSection } from "./SkillSection";
 import { SoldiersSection } from "./SoldiersSection";
 import { TalentSection } from "./TalentSection";
+import { Tabs, Tab } from "@material-ui/core";
+import { MaterialSection } from "./MaterialSection";
 
 export function HeroComponent({ hero }: { hero: Hero }) {
+  {
+    /* tab value*/
+  }
+  const [tab, setTab] = useState("materials");
+  const handleChangeTab = (event: React.ChangeEvent<{}>, newTab: string) => {
+    setTab(newTab);
+  };
+
   return (
     <div className="bg-white flex flex-grow justify-center flex-col">
       <h1 className="text-6xl text-center mb-10 font-thin text-gray-600">
@@ -37,9 +47,26 @@ export function HeroComponent({ hero }: { hero: Hero }) {
         </BoundedColumn>
       </div>
       <div className="flex flex-col">
-        <SoldiersSection hero={hero}></SoldiersSection>
-        <ClassSection heroClass={hero.startingClass}></ClassSection>
-        {hero.spClass && <ClassSection heroClass={hero.spClass}></ClassSection>}
+
+        <div className="flex flex-wrap justify-center">
+          <Tabs value={tab} onChange={handleChangeTab}>
+            <Tab label="Class & Soldiers" value="main" />
+            <Tab label="Materials" value="materials" />
+          </Tabs>
+        </div>
+
+        {tab === "main" && (
+          <Fragment>
+            <SoldiersSection hero={hero}></SoldiersSection>
+            <ClassSection heroClass={hero.startingClass}></ClassSection>
+            {hero.spClass && (
+              <ClassSection heroClass={hero.spClass}></ClassSection>
+            )}
+          </Fragment>
+        )}
+        {tab === "materials" && (
+          <MaterialSection heroClass={hero.startingClass} />
+        )}
       </div>
     </div>
   );
---
+++ components/heroes/HeroIcon.tsx
@@ -0,0 +1,24 @@
+import { BoundedColumn } from "../layout/BoundedColumn";
+import React from "react";
+
+export function HeroIcon({ name }: { name: string }) {
+  return (
+    <div className="flex flex-row bg-gray-200 justify-center">
+      <BoundedColumn>
+        <div className="flex- flex-row w-full ml-2 mr-2">
+          <div className="flex mt-2 mb-2 items-center justify-center sm:justify-start">
+            <img
+              src={"/classes/" + name + ".png"}
+              className="inline"
+              width={70}
+              height={75}
+            ></img>
+            <div>
+              <div className="ml-2 text-2xl">{name}</div>
+            </div>
+          </div>
+        </div>
+      </BoundedColumn>
+    </div>
+  );
+}
