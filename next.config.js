ad5f6de33f216a27c8669ad8e668c64ce0ba8382
RedWrath5 <ezchess9@gmail.com>
2021-11-07 08:41:54 -0600

    Remove image prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  assetPrefix: isProd ? "/RedWrath5/wikigrisser-next/" : "",
+  assetPrefix: isProd ? "/wikigrisser-next/" : "",
   future: {
     webpack5: true,
   },
