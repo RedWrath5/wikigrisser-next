ad4c8df37d9af3942166c2cbb9a45e6dc3a3f630
RedWrath5 <ezchess9@gmail.com>
2021-11-07 09:06:14 -0600

    Repair dev

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const isProd = process.env.NODE_ENV === "production";
+const isProd = false; //process.env.NODE_ENV === "production";
 
 module.exports = {
   assetPrefix: isProd ? "/wikigrisser-next/" : "",
