22e21d341c7ec38584b4118716c6f3633460602d
RedWrath5 <ezchess9@gmail.com>
2021-11-07 09:08:12 -0600

    Add base-path for gh-pages builds

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const isProd = false; //process.env.NODE_ENV === "production";
 
 module.exports = {
+  basePath: isProd ? "/wikigrisser-next/" : "",
   assetPrefix: isProd ? "/wikigrisser-next/" : "",
   future: {
     webpack5: true,
