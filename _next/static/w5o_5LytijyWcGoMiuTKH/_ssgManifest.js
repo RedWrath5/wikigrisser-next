3ddd54d402abbeb7f32616e349373afbab0f4629
RedWrath5 <RedWrath5@users.noreply.github.com>
2021-11-07 14:44:48 +0000

    deploy: ad5f6de33f216a27c8669ad8e668c64ce0ba8382

---
+++ _next/static/w5o_5LytijyWcGoMiuTKH/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fnews","\u002Fequipment","\u002Fheroes\u002Fgallery","\u002Fsoldiers","\u002Fheroes\u002F[name]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
